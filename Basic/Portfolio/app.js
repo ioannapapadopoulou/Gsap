9019d514182aafb194d21797e48d21c62606c6de
ioannapapadopoulou <59802278+ioannapapadopoulou@users.noreply.github.com>
2021-01-16 18:49:07 +0200

    Add files via upload

---
+++ Basic/Portfolio/app.js
@@ -0,0 +1,44 @@
+const navButton = document.querySelector('.nav-button');
+const navOpen = document.querySelector('.nav-open');
+
+
+const tl = new TimelineLite({ paused: true, reversed: true});
+
+const hello = () => {
+    window.alert('Start!');
+};
+
+tl.to('.cover', 1, {
+    width: '60%',
+    ease: Power2.easeOut,
+    onStart: hello,
+}).to(
+    'nav',
+     1, 
+     {
+      height: '100%',
+      ease: Power2.easeOut
+},
+'-=0.5'
+)
+.fromTo('.nav-open', 0.5, {  
+    opacity: 0,
+    x: 50,          //From
+    ease: Power2.easeOut
+}, {
+    opacity: 1,   //To
+    x: 0 ,
+    onComplete: function(){
+        navOpen.style.pointerEvents = 'auto';
+        console.log('done'); 
+    }  
+  }
+);
+
+navButton.addEventListener("click", () => {
+    toggleTween(tl)
+});
+
+function toggleTween(tween) {
+    tween.reversed() ? tween.play() : tween.reverse() 
+}
